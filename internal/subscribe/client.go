77ec73a0143126fa5367fdf48cc3ce0c7f37f01c
m.shakiba <M1shakiba>
2020-05-17 22:06:32 +0430

    added some features for subscribe

---
+++ internal/subscribe/client.go
@@ -0,0 +1,4 @@
+package subscribe
+
+type name struct {
+}
--- internal/subscribe/subscribe.go
+++ internal/subscribe/subscribe.go
@@ -3,33 +3,50 @@ package subscribe
 import "go-broker/internal/tcp"
 
 type SubscriberManager struct {
-	ackMessageChan chan<- string
-	nackMessageChan chan<- string
-	publishMessageChan <-chan
+	ackMessageChan     chan<- string
+	nackMessageChan    chan<- string
+	publishMessageChan <-chan PublishedMessage
+	clients            map[string][]string
 }
 
+type PublishedMessage struct {
+	Payload []byte
+	Routes  []string
+}
 
-
-func InitSubscriberManager(ackChan chan<- string, nackChan chan<- string, socketServer *tcp.Server) {
+func InitSubscriberManager(ackChan chan<- string, nackChan chan<- string, socketServer *tcp.Server, publishMessageChan <-chan PublishedMessage) {
 	mgr := SubscriberManager{
-		ackMessageChan:  ackChan,
-		nackMessageChan: nackChan,
+		ackMessageChan:     ackChan,
+		nackMessageChan:    nackChan,
+		publishMessageChan: publishMessageChan,
+		clients:            make(map[string][]string),
 	}
 
 	socketServer.RegisterHandler("SUB", mgr.handleSubscribeMessage)
 	socketServer.RegisterHandler("ACK", mgr.handleAckMessage)
 	socketServer.RegisterHandler("NCK", mgr.handleNackMessage)
 
+	go func() {
+		for {
+			msg := <-mgr.publishMessageChan
+
+		}
+	}()
 }
 
-func (s *SubscriberManager) handleSubscribeMessage(msgContext *tcp.MessageContext)  {
+func (s *SubscriberManager) handleSubscribeMessage(msgContext *tcp.MessageContext) {
 
 }
 
-func (s *SubscriberManager) handleAckMessage(msgContext *tcp.MessageContext)  {
+func (s *SubscriberManager) handleAckMessage(msgContext *tcp.MessageContext) {
 
 }
 
-func (s *SubscriberManager) handleNackMessage(msgContext *tcp.MessageContext)  {
+func (s *SubscriberManager) handleNackMessage(msgContext *tcp.MessageContext) {
 
+}
+
+func (s *SubscriberManager) processMessage(msg PublishedMessage) {
+	// check which client should receive the message
+	// enqueue the message in the client
 }
