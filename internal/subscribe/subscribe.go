b024899664690941a896547ef811004f6a0501c7
m.shakiba <work.shakiba@gmail.com>
2020-05-17 09:39:24 +0430

    added partial implementation of subscribe

---
+++ internal/subscribe/subscribe.go
@@ -0,0 +1,35 @@
+package subscribe
+
+import "go-broker/internal/tcp"
+
+type SubscriberManager struct {
+	ackMessageChan chan<- string
+	nackMessageChan chan<- string
+	publishMessageChan <-chan
+}
+
+
+
+func InitSubscriberManager(ackChan chan<- string, nackChan chan<- string, socketServer *tcp.Server) {
+	mgr := SubscriberManager{
+		ackMessageChan:  ackChan,
+		nackMessageChan: nackChan,
+	}
+
+	socketServer.RegisterHandler("SUB", mgr.handleSubscribeMessage)
+	socketServer.RegisterHandler("ACK", mgr.handleAckMessage)
+	socketServer.RegisterHandler("NCK", mgr.handleNackMessage)
+
+}
+
+func (s *SubscriberManager) handleSubscribeMessage(msgContext *tcp.MessageContext)  {
+
+}
+
+func (s *SubscriberManager) handleAckMessage(msgContext *tcp.MessageContext)  {
+
+}
+
+func (s *SubscriberManager) handleNackMessage(msgContext *tcp.MessageContext)  {
+
+}
